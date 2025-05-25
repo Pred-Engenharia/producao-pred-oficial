d3a6ae8fdb6046d1c3a8c6b93cfa79f286f34399
Yanick Santos <yanick@predengenharia.onmicrosoft.com>
2025-05-25 11:05:44 -0300

    nova alteração de privacidade

---
+++ pages/privacidade.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-main>
+        <v-container>
+            <div class="h-screen pa-10">
+                <h1 class="text-h3 font-weight-bold">Política de Privacidade</h1>
+                <div class="mt-10 text-body-2">
+                    <p>Este site e todo o seu conteúdo (“Site”) é de responsabilidade e operado pela <span class="font-weight-bold">PRED ENGENHARIA LTDA</span>.
+                        , sociedade empresária limitada, com sede na R. Italina Pereira Mota, 440 - Jardim Camburi, 
+                        Vitória - ES, 29090-370, inscrita no <span class="font-weight-bold">CNPJ sob o n° 39.642.624/0001-48</span> , com o e-mail de contato:  
+                        <a href="mailto:privacidade@pred.com.br" style="text-decoration: none;"  class="">privacidade@pred.com.br</a></p>
+
+                    
+                    <p class="mt-4">A Pred se preocupa com a privacidade e com a segurança dos dados de nossos clientes e por isso, 
+                        pautados na <span class="font-weight-bold">Lei Geral de Proteção de Dados Pessoais (LGPD) – Lei 13.709/2018</span>, preparamos esta 
+                        política de privacidade para que você, titular de dados pessoais, tenha acesso, com total 
+                        transparência, a informações sobre as atividades de tratamento que realizamos e sobre seus 
+                        direitos. </p>
+
+                    <p class="mt-4">Esta política de privacidade fornece informações relevantes sobre privacidade de dados, 
+                        incluindo sobre as suas escolhas e seus direitos como cliente, referentes à coleta, utilização e 
+                        compartilhamento de certas informações, como suas informações pessoais, pela Pred.</p>
+                </div>
+                <div class="mt-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">INFORMAÇÕES COLETADAS</h2>
+                    <p class="mt-6">A Pred  somente coletará dados pessoais de indivíduos que naveguem em nosso site ou que utilizem 
+                        os nossos serviços. Nós coletamos o mínimo possível de dados pessoais necessários para alcançar 
+                        as finalidades para as quais tais informações foram coletadas. A Pred somente utiliza esses dados 
+                        para finalidades específicas e legalmente autorizadas, conforme detalhado abaixo:</p>
+                </div>
+
+
+            </div>
+        </v-container>
+    </v-main>
+    
+</template>

e447bd39ef8df613407ee7eb6d28b267784c8b15
Yanick Santos <yanick@predengenharia.onmicrosoft.com>
2025-05-25 23:33:37 -0300

    verificar tabela mobile

--- pages/privacidade.vue
+++ pages/privacidade.vue
@@ -1,7 +1,7 @@
 <template>
     <v-main>
         <v-container>
-            <div class="h-screen pa-10">
+            <div class="">
                 <h1 class="text-h3 font-weight-bold">Política de Privacidade</h1>
                 <div class="mt-10 text-body-2">
                     <p>Este site e todo o seu conteúdo (“Site”) é de responsabilidade e operado pela <span class="font-weight-bold">PRED ENGENHARIA LTDA</span>.
@@ -27,6 +27,90 @@
                         as finalidades para as quais tais informações foram coletadas. A Pred somente utiliza esses dados 
                         para finalidades específicas e legalmente autorizadas, conforme detalhado abaixo:</p>
                 </div>
+
+                <!-- SESSÃO TABELA -->
+                 <div class="mt-10">
+                    <!--HEADER-->
+                    <div class="d-flex border bg-indigo-darken-4" >
+                        <div class="w-25 border px-4 pt-1">
+                            <h1 class="text-md-body-1 font-weight-bold text-caption">Finalidade</h1>
+                        </div>
+                         <div class="w-50 border px-4 pt-1">
+                            <h1 class="text-md-body-1 font-weight-bold text-caption">Dados Pessoais</h1>
+                        </div>
+                         <div class="w-25 border px-4 pt-1">
+                            <h1 class="text-md-body-1 font-weight-bold text-caption">Base Legal</h1>
+                        </div>
+                    </div>
+
+                    <!-- BODY -->
+                     <div class="d-flex">
+                        <div class="w-25 border px-4 py-2 text-caption"><p>Contato</p></div>
+                        <div class="w-50 border px-4 py-2 text-caption">
+                            <p>Contato	Nome completo, nº de telefone, endereço de e-mail	Consentimento ou Execução de contrato</p>
+                        </div>
+                        <div class="w-25 border px-4  py-2 text-caption">
+                            <p >Consentimento ou Execução de contrato</p>
+                        </div>
+                     </div>
+
+
+                     <div class="d-flex">
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Contratação de nossos serviços ou contratação de fornecedores</p>
+                        </div>
+                        <div class="w-50 border px-4 py-2 text-caption">
+                            <p>Nome completo, estado civil, profissão, nacionalidade, endereço residencial, endereço de e-mail, nº de CPF, nº de telefone e assinatura.</p>
+                        </div>
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Procedimentos preliminares para contratação de serviço</p>
+                        </div>
+                     </div>
+
+
+                     <div class="d-flex">
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Prestação dos serviços contratados</p>
+                        </div>
+                        <div class="w-50 border px-4 py-2 text-caption">
+                            <p>Nome completo, nº de telefone, endereço de e-mail e nº de CPF.</p>
+                        </div>
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Execução de contrato</p>
+                        </div>
+                     </div>
+
+
+                     <div class="d-flex">
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Permitir o acesso ao nosso site e analisar a navegação de usuários em nosso site (apenas para o blog)</p>
+                        </div>
+                        <div class="w-50 border px-4 py-2 text-caption">
+                            <p>Cookies Dados de navegação (apenas para o blog): IP, data e hora de acesso, localização geográfica, idioma, duração do acesso, páginas visitadas, tipo e versão do navegador.
+                            Armazenamento de preferências de cookies, conforme exigido pela própria LGPD.
+                            Armazenamento de interações para fins de campanhas do Google Ads (apenas para a página do blog)
+                            </p>
+                        </div>
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Cumprimento de obrigação legal ou regulatória: para a identificação, registro do Endereço de IP e gestão de preferências do usuário.
+                            Consentimento – para cookies não essenciais.
+                            </p>
+                        </div>
+                     </div>
+
+                     <div class="d-flex">
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Para acesso ao Portal MCI por clientes</p>
+                        </div>
+                        <div class="w-50 border px-4 py-2 text-caption">
+                            <p>Nome completo, nº de telefone e endereço de e-mail.</p>
+                        </div>
+                        <div class="w-25 border px-4 py-2 text-caption">
+                            <p>Execução de contrato</p>
+                        </div>
+                     </div>
+
+                 </div>
 
 
             </div>

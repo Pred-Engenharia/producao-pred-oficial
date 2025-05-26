4671b9454c121520c45e46eaec4a6f9a69344f79
Yanick Santos <yanick@predengenharia.onmicrosoft.com>
2025-05-26 00:33:41 -0300

    primeira versao - tela de privacidade completa

--- pages/privacidade.vue
+++ pages/privacidade.vue
@@ -1,7 +1,7 @@
 <template>
     <v-main>
         <v-container>
-            <div class="">
+            <div class="py-10">
                 <h1 class="text-h3 font-weight-bold">Política de Privacidade</h1>
                 <div class="mt-10 text-body-2">
                     <p>Este site e todo o seu conteúdo (“Site”) é de responsabilidade e operado pela <span class="font-weight-bold">PRED ENGENHARIA LTDA</span>.
@@ -112,7 +112,86 @@
 
                  </div>
 
+                 <div class="mt-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">PARA QUE COLETAMOS DADOS MEDIANTE COOKIES?</h2>
+                    <p class="mt-6">Algumas informações podem ser voluntariamente fornecidas por você, como, por exemplo, dados de contato ou dados cadastrais. No entanto, quando um usuário acessa nosso site ou aplicativo, alguns dados pessoais podem ser coletados de forma automática, por meio de cookies e outras tecnologias de monitoramento assemelhadas.</p>
+                    <p class="mt-6">Cookies são pequenos arquivos que sites enviam para o seu navegador e que são armazenados no seu dispositivo, permitindo a coleta de informações a seu respeito de maneira automática, como, por exemplo, seu idioma ou sua recorrência de acesso a determinado site.</p>
+                    <p class="mt-6">Nosso site utiliza os seguintes tipos de cookies:</p>
+                    <div>
+                        <ul class="ml-8 my-8">
+                            <li class="mb-6"><span class="font-weight-bold">Cookies essenciais – </span> <span>são utilizados para coletar informações que permitam o correto e adequado funcionamento do nosso site (por exemplo, são usados para identificar os usuários, garantir o controle de verificações e para ajustar a exibição do conteúdo ao equipamento).</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">Cookies analíticos (analytics) – </span> <span>são aqueles utilizados para coletar informações sobre a sua utilização do nosso site, para podermos avaliar como são suas interações, para melhorar nossos serviços e sua experiência durante o acesso ao nosso site (esses cookies capturam, por exemplo, com que frequência um usuário acessa nosso site, além de informações sobre cliques de usuários nas páginas de nosso site).</span></li>
+                            <li><span class="font-weight-bold">Cookies de preferências – </span> <span>são aqueles utilizados para coletar informações que nos permitam verificar quais são seus interesses e preferências, a fim de que possamos oferecer os nossos serviços que sejam relacionados aos seus interesses. Esses cookies coletam informações como, por exemplo, qual o tipo de conteúdo você mais acessa ou quais as suas preferências durante a navegação no site.</span></li>
+                        </ul>
+                    </div>
+                    
 
+                    <p class="mt-6">Com exceção dos cookies essenciais, os quais não podem ser desabilitados, pois isso inviabilizaria o seu acesso ao nosso site, você poderá, a qualquer tempo, desativar o uso dos demais cookies ou até mesmo deletá-los, por meio da realização de ajustes nas configurações de seu navegador ou dispositivo. Recomendamos que você visite o site do navegador que utiliza ou do fabricante de seu dispositivo, para obter maiores informações sobre como realizar esse procedimento de eliminação.</p>
+                    <p class="mt-6">Nós realizamos a coleta automatizada de informações analíticas por meio do serviço de web analytics oferecido pelo Google (Google Analytics), que por meio de cookies analíticos identificam os seus padrões de navegação no nosso site e gera relatórios sobre suas atividades.</p>
+                    <p class="mt-6">Assim, é importante que você saiba que todas essas informações coletadas por cookies analíticos são, consequentemente, compartilhadas com o Google e Facebook em razão da prestação do serviço de web analytics. Nós recomendamos que você leia atentamente a política de privacidade do Google, disponível no link <a href="https://policies.google.com/privacy" target="_blank">https://policies.google.com/privacy</a> , para entender sobre como o Google trata essas informações coletadas por meio cookies de analíticos, bem como entender a forma pela qual você pode atualizar, gerenciar, exportar e excluir os dados de sua titularidade.</p>
+                 </div>
+
+                 <div class="mt-10 text-body-2">
+                     <h2 class="text-h6 font-weight-bold">COMPARTILHAMENTO DE DADOS PESSOAIS</h2>
+                     <p class="mt-6">Todas as informações coletadas sobre você são confidenciais. Em hipótese alguma nós venderemos as suas informações. No entanto, podemos compartilhá-las com terceiros, caso seja necessário para:</p>
+
+                     <div>
+                        <ul class="ml-8 my-8">
+                            <li class="mb-6"><span class="font-weight-bold">prestação dos serviços que ofertamos a você – </span> <span>seus dados pessoais podem ser compartilhados com alguns de nossos parceiros e fornecedores, a fim de viabilizarmos a prestação dos serviços contratados por você, por exemplo, empresas que prestem serviços de hospedagem de dados para armazenamento da nossa base de dados, empresas que hospedem nosso site e nossos e-mails, empresas que nos apoiem no desenvolvimento de software e outras tecnologias, empresas que ofereçam meios de pagamentos para processarmos o seu pagamento quando da compra de nossos Serviços, entre outras.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">acesso ao site – </span> <span>podemos compartilhar informações com alguns de nossos parceiros e fornecedores de serviços de web analytics, para possibilitar que você acesse nosso site sem problemas e para que nós, a partir de uma avaliação das suas interações, possamos compreender como melhorar nossos serviços e a sua experiência durante o uso de nosso site.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">oferecimento de ofertas e oportunidades – </span> <span>seus dados pessoais também podem ser compartilhados com alguns de nossos parceiros e fornecedores de serviços para entendermos quais são seus interesses e oferecermos nossos próprios serviços e outras oportunidades que estejam de acordo com o seu perfil.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">resguardo de direitos – </span> <span>nós poderemos compartilhar seus dados pessoais com terceiros, caso seja necessário para resguardar os nossos direitos e interesses, cumprir uma obrigação legal ou regulatória ou atender a uma ordem judicial (por exemplo, com escritórios de advocacia, escritórios de contabilidade, Poder Judiciário, Ministério Público, Órgãos de Defesa do Consumidor, autoridades da Administração Pública, entre outros).</span></li>
+                            <li><span class="font-weight-bold">reestruturação Societária – </span> <span>suas informações são parte de nosso ativo empresarial e, portanto, poderão ser compartilhadas com outras empresas do nosso grupo econômico, no estrito limite necessário, nos casos em que houver transações e reestruturações societárias envolvendo a Pred, como, por exemplo, fusão, aquisição, incorporação e alteração de controle, hipótese na qual a transferência das suas informações será fundamental à continuidade dos serviços prestados.</span></li>
+                        </ul>
+                    </div>
+
+                    <p class="mt-6">Em todos os casos, os seus Dados Pessoais somente serão compartilhados com terceiros apenas para atender as finalidades aqui descritas e no estrito limite necessário para que tal finalidade seja atingida. A Pred, sempre que possível, irá pseudonimizar os seus dados pessoais antes do compartilhamento e solicitará aos terceiros que mantenham a confidencialidade e a segurança das informações compartilhadas.</p>
+                    <p class="mt-6">No curso do compartilhamento de dados, sempre que transferirmos informações pessoais a países fora do território nacional e outras regiões com leis abrangentes de proteção de dados, nós nos certificaremos de que as informações sejam transferidas segundo esta política de privacidade e conforme permitido pela legislação de proteção de dados aplicável.</p>
+                 </div>
+
+                 <div class="mt-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">MEDIDAS DE SEGURANÇA</h2>
+                    <p class="mt-6">Nós levamos sua privacidade a sério e tomamos as medidas e precauções razoáveis para proteger seus dados pessoais. Trabalhamos para protegê-lo contra acessos não autorizados, alterações acidentais ou ilícitas, divulgações ou destruições de seus dados pessoais.</p>
+                    <p class="mt-6">A Pred emprega medidas administrativas, lógicas e físicas razoáveis para proteger suas informações pessoais, essas medidas são elaboradas para oferecer um nível de segurança adequado aos riscos de processamento de suas informações pessoais, incluindo: as conexões via internet são protegidas por firewall, utilização de ferramenta de antivírus, proteção por criptografia de nossas comunicações e de dados armazenados em nossos dispositivos, além disso também realizamos backups periódicos de nossa base de dados.</p>
+                    <p class="mt-6">Todos os dados pessoais são tratados como informações confidenciais e o seu acesso por terceiros é restrito, sendo permitido apenas às pessoas previamente autorizadas (sejam as empresas com quem os compartilhamos ou nossos empregados e colaboradores), que estão obrigados ao dever sigilo e sendo vedado o seu uso de forma inadequada, em desacordo com o disposto nesta Política de Privacidade.</p>
+                    <p class="mt-6">A segurança de seus dados pessoais também é de sua responsabilidade, assegure que o ambiente de seu computador ou dispositivo de acesso ao nosso site permaneça seguro, a partir (i) do uso de ferramentas adequadas, como, por exemplo, antivírus e firewall, (ii) da utilização de versões atualizadas de navegadores, sistemas operacionais e demais software aplicáveis, bem como (iii) do não compartilhamento de seu login e senha com terceiros. A Pred não solicitará, por e-mail, telefone ou WhatsApp, informações relativas a suas senhas de acesso ou quaisquer informações pessoais a seu respeito. Caso você suspeite que os seus dados pessoais estejam em risco, você deve entrar em contato conosco. </p>
+                 </div>
+
+                 <div class="mt-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">DIREITOS DOS TITULARES DE DADOS</h2>
+                    <p class="mt-6">Você possui direitos inerentes a sua condição de titular de dados pessoais. Esses direitos visam proteger suas informações e resguardar a sua privacidade. Listamos abaixo quais são esses direitos e como você pode exercê-los: </p>
+                
+                    <div>
+                        <ul class="ml-8 my-8">
+                            <li class="mb-6"><span class="font-weight-bold">Existência e acesso – </span> <span>você tem o direito de confirmar a realização de algum tratamento com seus dados pessoais, bem como ter acesso aos dados pessoais de sua titularidade tratados por nós, mediante a solicitação e o recebimento de um documento contendo a cópia integral de seus dados pessoais e o respectivo tratamento realizado com ele.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">Retificação – </span> <span>você tem o direito de solicitar a correção ou a atualização de seus dados pessoais, caso verifique que eles estão incorretos ou desatualizados.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">Explicação –  </span> <span>você tem o direito de obter informações sobre o tratamento realizado com seus dados pessoais, como, por exemplo, quais dados pessoais seus são coletados, como eles são utilizados, com quem são compartilhados e por que, entre outras informações. Essa Política de Privacidade visa oferecer todas as informações necessárias para garantir que esse direito seja atendido. Caso Você não compreenda alguma questão aqui abordada, ou identifique a omissão de algum assunto, você pode solicitar esclarecimentos adicionais à Pred.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">Eliminação –  </span> <span>você tem o direito de solicitar a exclusão de seus dados pessoais da nossa base de dados, de forma definitiva, ou solicitar que eles sejam anonimizados ou, ainda, requerer que o tratamento dos seus dados pessoais seja impedido. Sempre atenderemos suas requisições, exceto com relação aos pedidos de exclusão e anonimização, quando a manutenção do dado pessoal for necessária para que possamos resguardar nossos direitos e/ou cumprir obrigações legais ou regulatórias. Você está ciente de que nos casos de exclusão, anonimização e bloqueio, não poderemos prestar nossos serviços a você.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">Oposição – </span> <span>você tem o direito de contestar determinado tratamento de seus dados pessoais, bem como de retirar o seu consentimento, quando o tiver fornecido para que pudéssemos conduzir determinada atividade de tratamento sobre seus dados pessoais.</span></li>
+                            <li class="mb-6"><span class="font-weight-bold">Portabilidade – </span> <span>você tem o direito de solicitar que os dados pessoais de sua titularidade lhe sejam enviados, ou enviados aos terceiros que você indicar, em formato estruturado e interoperável.</span></li>
+                            <li><span class="font-weight-bold">Revisão de decisões automatizadas –  </span> <span>você tem o direito de solicitar a revisão de decisões que tenham afetado seus interesses e tenham sido tomadas a partir de um tratamento exclusivamente automatizado de seus dados pessoais. Embora essa prerrogativa esteja prevista na legislação, as decisões tomadas pela Pred não são automatizadas.</span></li>   
+                        </ul>
+                    </div>
+                    <p class="mt-6">Além de exercer os direitos aqui descritos, você pode escolher por não fornecer determinados dados pessoais para nós (como, por exemplo, no caso de cookies). No entanto, alguns desses dados são essenciais para a prestação dos serviços e acesso ao nosso site, sem os quais você não poderá não ser capaz de utilizar determinados recursos.</p>
+                    <p class="mt-6">Você poderá exercer qualquer um dos direitos aqui descritos, a qualquer momento, por procedimento gratuito e facilitado. Para tanto, basta encaminhar uma solicitação para nós por meio do endereço de e-mail:  <a href="mailto:privacidade@pred.com.br" style="text-decoration: none;"  class="">privacidade@pred.com.br</a> Nós empregaremos nossos melhores esforços para atender a todos os pedidos no menor espaço de tempo possível.</p>
+                </div>
+
+                 <div class="mt-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">PERÍODO DE ARMAZENAMENTO DOS DADOS</h2>
+                    <p class="mt-6">Todos os dados pessoais coletados pela Pred são armazenados pelo período necessário para atender as finalidades descritas nesta Política de Privacidade ou até que você, como titular, exerça os seus direitos de oposição ou cancelamento, solicitando a exclusão do dado pessoal ou retirando o seu consentimento, quando aplicável.</p>
+                    <p class="mt-6">Uma vez que determinados dados pessoais não sejam mais necessários ou relevantes para a finalidade específica ou que você exerça, como titular, os direitos aqui mencionados, eles serão excluídos de nossa base de dados. Todavia, mesmo que a finalidade tenha sido concluída ou que você tenha retirado seu consentimento ou solicitado a exclusão de seus dados pessoais, a Pred  poderá mantê-los armazenados, se necessário para resguardar nossos direitos, cumprir ordens judiciais ou requisições emanadas de autoridades competentes, bem como para cumprir de obrigações legais ou regulatórias (como, por exemplo, para fins de armazenamento obrigatório dos registros de acesso ao nosso site).</p>
+                </div>
+
+                 <div class="mt-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">POSSIBILIDADE DE ALTERAÇÃO DESTA POLÍTICA</h2>
+                    <p class="mt-6">A Pred poderá alterar periodicamente esta política de privacidade para atender a mudanças na legislação, exigências regulatórias ou operacionais. Comunicaremos qualquer alteração (inclusive a data em que esta entrar em vigor) conforme previsto por lei. Caso você não queira reconhecer ou aceitar nenhuma das atualizações desta política de privacidade, você poderá assim nos informar através do e-mail: <a href="mailto:privacidade@pred.com.br" style="text-decoration: none;"  class="">privacidade@pred.com.br</a>.</p>
+                 </div>
+
+                 <div class="my-10 text-body-2">
+                    <h2 class="text-h6 font-weight-bold">COMO REALIZAR SOLICITAÇÕES ENVOLVENDO DADOS PESSOAIS?</h2>
+                    <p class="mt-6">A Pred  está à disposição para esclarecer qualquer questão relacionada às atividades de tratamento que conduzimos sobre seus dados pessoais. Em caso de dúvidas, solicitações, problemas, reclamações ou sugestões relacionadas a esta Política de Privacidade, ao tratamento de seus dados pessoais ou o exercício de seus direitos como titular de dados, você pode entrar em contato com nosso Encarregado de Proteção de Dados (Daniella Moraes Santos), a partir do seguinte endereço de e-mail:  <a href="mailto:privacidade@pred.com.br" style="text-decoration: none;"  class="">privacidade@pred.com.br</a>.</p>
+                    <p class="mt-6">A Pred  empregará os melhores esforços para responder a todos os e-mails no menor espaço de tempo possível. Se você entrar em contato conosco, para sua segurança e da Pred, poderá ser preciso autenticar sua identidade antes de atendermos a sua solicitação.</p>
+                </div>
             </div>
         </v-container>
     </v-main>
